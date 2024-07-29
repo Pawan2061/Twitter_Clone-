a7128c301065b4a9e43c8d0f0a810eb22079ec9c
Pawan2061 <pandeypawan2061@gmail.com>
2024-07-29 17:26:28 +0530

    feat: adding posts in schema

---
+++ prisma/migrations/20240729115535_post/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Tweet" (
+    "id" SERIAL NOT NULL,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Tweet" ADD CONSTRAINT "Tweet_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
